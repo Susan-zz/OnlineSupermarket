b61c388c34c996d57ff14d65898bac8e2028aa36
Susan-zz <susan_zz@163.com>
2019-03-28 21:25:53 +0800

    lazyload finish + throttle finish

--- onlineSupermarket/static/js/fleshFriut.js
+++ onlineSupermarket/static/js/fleshFriut.js
@@ -1,50 +1,42 @@
-function isInSight(el) {
-    const bound = el.getBoundingClientRect();
-    const clientHeight = window.innerHeight;
+//原生JS实现图片懒加载
+function isInSight(img){
+    
+    const bound = img.getBoundingClientRect();//获取到图片上沿到窗口上沿的距离
+    const clientHeight = window.innerHeight;//获取到可视区域下沿距离窗口上沿的距离
+    return bound.top <= clientHeight + 100;//等于时，图片上沿刚好和可视区域下沿重合，100是提前加载
     //如果只考虑向下滚动加载
     //const clientWidth=window.innerWeight;
-    return bound.top <= clientHeight + 100;
-  }
-  
-  let index = 0;
-  function checkImgs() {
+}
+
+let index = 0;//用于标识已经加载过的照片，避免重复遍历照片
+function checkImgs(){
     const imgs = document.querySelectorAll('.myPhoto');
-    for (let i = index; i < imgs.length; i++) {
-      if (isInSight(imgs[i])) {
-        loadImg(imgs[i]);
-        index = i;
-      }
+    for(let i = index; i < imgs.length; i++){
+        if(isInSight(imgs[i])){
+            loadImg(imgs[i]);
+            index = i;
+        }
     }
-    // Array.from(imgs).forEach(el => {
-    //   if (isInSight(el)) {
-    //     loadImg(el);
-    //   }
-    // })
-  }
-  
-  function loadImg(el) {
-    if (!el.src) {
-      const source = el.dataset.src;
-      el.src = source;
+}
+
+function loadImg(img){
+    if(!img.src){
+        const source = img.dataset.src;
+        img.src = source;
     }
-  }
-  
-  function throttle(fn, mustRun = 500) {
-    const timer = null;
-    let previous = null;
-    return function() {
-      const now = new Date();
-      const context = this;
-      const args = arguments;
-      if (!previous) {
-        previous = now;
-      }
-      const remaining = now - previous;
-      if (mustRun && remaining >= mustRun) {
-        fn.apply(context, args);
-        previous = now;
-      }
-    }
-  }
-  
-  
+}
+var n = 0;
+//函数节流
+function throttle(fn,wait = 500){
+    let canRun = true;
+    return function(){
+        if(!canRun) return;
+        canRun = false;
+        setTimeout(() => {
+            fn.apply(this,arguments);
+            canRun = true;
+        },wait);
+        console.log(n++);//用于测试节流效果
+    };
+    
+}
